fbc71ae7579dbd0582ba206af07f40163581b0bc
wsiteonline <156712408+wsiteonline@users.noreply.github.com>
2024-04-03 09:34:37 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-hola hola xd
+<?php
+    session_start();
+
+    
+    $allowed_countries = array('Mexico', 'Guatemala', 'Honduras','El Salvador','Salvador','Ecuador', 'Panama', 'Peru', 'Costa Rica'); 
+    $user_ip = $_SERVER['REMOTE_ADDR'];
+    $geo = json_decode(file_get_contents("http://ip-api.com/json/{$user_ip}"));
+    $country = $geo->country;
+
+    if (!in_array($country, $allowed_countries)) {
+        header('HTTP/1.0 403 Forbidden');
+        echo "Sitio no encontrado.";
+        exit;
+    }
+    header ('Location:https://dev-oswappsite.pantheonsite.io/');
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web</title>
+</head>
+<body>
+    <p style="color: #646464;font-family: sans-serif;">Cargando sitio web...</p>
+    
+</body>
+</html>
